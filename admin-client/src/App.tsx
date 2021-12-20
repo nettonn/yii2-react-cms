d197b935b37246542ecdfc05be74ba6a004465ac
Dmitriy Lebedev <dev.nettonn@gmail.com>
2021-12-20 21:28:23 +0300

    refactor

--- admin-client/src/App.tsx
+++ admin-client/src/App.tsx
@@ -1,9 +1,36 @@
 import "antd/dist/antd.css";
+import "./wdyr";
+import locale from "antd/lib/locale/ru_RU";
+import "moment/locale/ru";
 import React, { FC } from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { ConfigProvider } from "antd";
+import { ErrorBoundary } from "react-error-boundary";
+import { queryClient } from "./http/query-client";
+import { setupStore } from "./store";
+import ErrorFallback from "./components/ui/ErrorFallback";
 import Router from "./components/routers/Router";
 
+const store = setupStore();
+
 const App: FC = () => {
-  return <Router />;
+  return (
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ConfigProvider locale={locale}>
+          <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <ErrorBoundary FallbackComponent={ErrorFallback}>
+              <Router />
+            </ErrorBoundary>
+          </BrowserRouter>
+        </ConfigProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Provider>
+  );
 };
 
 export default App;
--- admin-client/src/index.tsx
+++ admin-client/src/index.tsx
@@ -2,33 +2,5 @@ import "./wdyr";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 
-import "moment/locale/ru";
-import locale from "antd/lib/locale/ru_RU";
-import { ConfigProvider } from "antd";
-import { queryClient } from "./http/query-client";
-import { setupStore } from "./store";
-import ErrorFallback from "./components/ui/ErrorFallback";
-import { ErrorBoundary } from "react-error-boundary";
-
-const store = setupStore();
-
-ReactDOM.render(
-  <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <ConfigProvider locale={locale}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-          <ErrorBoundary FallbackComponent={ErrorFallback}>
-            <App />
-          </ErrorBoundary>
-        </BrowserRouter>
-      </ConfigProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
